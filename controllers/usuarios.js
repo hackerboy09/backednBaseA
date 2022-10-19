e8aa1a62f94a24a0c2fe98218adf770d5079bac6
hackerboy09 <isc19350319@gmail.com>
2022-10-19 10:08:20 -0500

    Buscar usuarios en la base de datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -24,4 +24,28 @@ const getUsers = async (req = request, res = response) => {
     }
 }
 
-module.exports = {getUsers}
+const getUserByID = async (req = request, res = response) => {
+    const {id} = req.params
+    let conn;
+
+    try {
+        conn = await pool.getConnection()
+
+        const [user] = await conn.query(`SELECT * FROM Usuarios WHERE ID =${id}` , (error) => { throw new Error(error)})
+
+        if(!user){
+            res.status(404).json({msg: `No se encontraron registros con el ID ${id}`})
+            return
+        }
+        res.json({user})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    } finally {
+        if(conn){
+            conn.end()
+        }
+    }
+}
+
+module.exports = {getUsers, getUserByID}
